29c90ac2bce9bf680f63cc41c14fc623dc78b868
akopyan-arsen <tigrikpictures@gmail.com>
2024-03-30 15:16:51 +0200

    hw1 done

--- src/js/1-timer.js
+++ src/js/1-timer.js
@@ -1,12 +1,85 @@
 import flatpickr from 'flatpickr';
 import 'flatpickr/dist/flatpickr.min.css';
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+
+const startBtn = document.querySelector('button[data-start]');
+const input = document.querySelector('#datetime-picker');
+const selectedDay = document.querySelector('.value[data-days]');
+const selectedHour = document.querySelector('.value[data-hours]');
+const selectedMinute = document.querySelector('.value[data-minutes]');
+const selectedSecond = document.querySelector('.value[data-seconds]');
+
+let userSelectedDate = null;
+let intervalId = null;
+startBtn.disabled = true;
+
 const options = {
   enableTime: true,
   time_24hr: true,
   defaultDate: new Date(),
   minuteIncrement: 1,
   onClose(selectedDates) {
-    console.log(selectedDates[0]);
+    if (selectedDates[0].getTime() > Date.now()) {
+      userSelectedDate = selectedDates[0].getTime();
+      startBtn.disabled = false;
+    } else {
+      startBtn.disabled = true;
+      return iziToast.error({
+        message: 'Please choose a date in the future',
+        position: 'topRight',
+        timeout: 0,
+        backgroundColor: '#EF4040',
+        messageColor: '#fff',
+        timeout: 1500,
+      });
+    }
   },
 };
+
 flatpickr('#datetime-picker', options);
+
+function start() {
+  if (userSelectedDate === null) {
+    return;
+  }
+  startBtn.disabled = true;
+  input.disabled = true;
+  intervalId = setInterval(() => {
+    const currentTime = Date.now();
+    let elapsedTime = userSelectedDate - currentTime;
+    elapsedTime = Math.max(elapsedTime, 0);
+    updateClockface(convertMs(elapsedTime));
+    if (elapsedTime <= 0) {
+      clearInterval(intervalId);
+      startBtn.disabled = false;
+      input.disabled = false;
+    }
+  }, 1000);
+}
+
+function convertMs(ms) {
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  const days = addLeadingZero(Math.floor(ms / day));
+  const hours = addLeadingZero(Math.floor((ms % day) / hour));
+  const minutes = addLeadingZero(Math.floor((ms % hour) / minute));
+  const seconds = addLeadingZero(Math.floor((ms % minute) / second));
+  return { days, hours, minutes, seconds };
+}
+
+function addLeadingZero(value) {
+  return String(value).padStart(2, '0');
+}
+
+function updateClockface({ days, hours, minutes, seconds }) {
+  selectedDay.innerHTML = `${days}`;
+  selectedHour.innerHTML = `${hours}`;
+  selectedMinute.innerHTML = `${minutes}`;
+  selectedSecond.innerHTML = `${seconds}`;
+}
+
+startBtn.addEventListener('click', start);
