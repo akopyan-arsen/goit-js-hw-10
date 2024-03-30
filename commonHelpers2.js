6bf04c935e7a918f7baf6b2e4cbaaf1885236bb3
akopyan-arsen <149099397+akopyan-arsen@users.noreply.github.com>
2024-03-30 22:46:12 +0000

    Deploying to gh-pages from @ akopyan-arsen/goit-js-hw-10@d40409cfc5cbb5faab8d3f317b2aaa66169049bf 🚀

---
+++ commonHelpers2.js
@@ -0,0 +1,2 @@
+import"./assets/styles-36dfb935.js";import"./assets/vendor-77e16229.js";const l=document.querySelector('button[type="submit"]'),i=document.querySelectorAll('input[type="ratio"]');l.addEventListener("click",u);let t=null;function u(){input.value<0||(i.forEach(e=>{e.checked&&(t=e.value)}),promise=new Promise((e,n)=>{t!==null?e(t):n(t)}))}
+//# sourceMappingURL=commonHelpers2.js.map
